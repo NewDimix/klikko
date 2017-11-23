99ec761924ca06e54b2386e492619d3bbaf187d7
ND <dimix93@mail.ru>
2017-11-23 17:19:35 +0600

    menu overflow fixed

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,19 @@
 $(window).on('load', function () {
   var isMobile = $('.js-menu-btn').is(":visible");
-  $('.js-menu-btn').on('click', function () {
-    $(this).toggleClass('open');
-    $('.js-menu-content').toggleClass('open');
+
+  $('.js-menu-btn').click(function (event) {
+    $('.js-menu-content').slideToggle();
+    event.stopPropagation();
+    $('.js-menu-btn').toggleClass('open');
+  });
+
+  $(document).mouseup(function (e) {
+    var container = $('.js-menu');
+    if (container.has(e.target).length === 0) {
+      if ($(window).width() < '768') {
+        $('.js-menu-content').slideUp();
+        $('.js-menu-btn').removeClass('open');
+      }
+    }
   });
 });
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,1 @@
-$(window).on("load",function(){$(".js-menu-btn").is(":visible");$(".js-menu-btn").on("click",function(){$(this).toggleClass("open"),$(".js-menu-content").toggleClass("open")})});
+$(window).on("load",function(){$(".js-menu-btn").is(":visible");$(".js-menu-btn").click(function(n){$(".js-menu-content").slideToggle(),n.stopPropagation(),$(".js-menu-btn").toggleClass("open")}),$(document).mouseup(function(n){0===$(".js-menu").has(n.target).length&&$(window).width()<"768"&&($(".js-menu-content").slideUp(),$(".js-menu-btn").removeClass("open"))})});
